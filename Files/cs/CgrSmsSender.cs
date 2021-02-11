0346e4d2352e7e67870b3d490af2647f52ea476b
IceVenOm98 <IceVenOm98@gmail.com>
2021-02-11 12:39:40 +0300

    Добавьте файлы проекта.

---
+++ Files/cs/CgrSmsSender.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace CgrNewPackage
+{
+
+    using Terrasoft.Core.Factories;
+
+    [DefaultBinding(typeof(ICgrSmsSender))]
+    class CgrSmsSender : ICgrSmsSender
+    {
+        public int SendSms(string messageAndNumber)
+        {
+            string pattern = @"^[а-яА-Яa-zA-Z0-9.!?]*;\+[0-9]*$";
+            if (Regex.IsMatch(messageAndNumber, pattern, RegexOptions.IgnoreCase))
+            {
+
+                string[] messageData = messageAndNumber.Split(";".ToCharArray()[0]);
+                // Find your Account Sid and Token at twilio.com/console
+                // and set the environment variables. See http://twil.io/secure
+                string accountSid = "AC4fab17a83dc129b26ecd7155cc0473c0";   //Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+                string authToken = "54a5f45603f499e34e3c56f7dbc84925";      //Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+                TwilioClient.Init(accountSid, authToken);
+
+                var message = MessageResource.Create(
+                    body: messageData[0],
+                    from: new Twilio.Types.PhoneNumber("+18144202870"),
+                    to: new Twilio.Types.PhoneNumber(messageData[1])
+                );
+                string str = message.Sid;
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Terrasoft.Core.Attributes;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: PackageReferenceAssembly(RefAssemblyMarker.All)]
+[assembly: AssemblyTitle("CgrNewPackage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CgrNewPackage")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cb66e314-d6c8-49fe-9b94-8e6be89ea074")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
